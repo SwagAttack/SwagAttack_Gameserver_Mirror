461dd9c6da9408f899d662211dc0354f49c6b312
nicho1991 <ladefoged1991@gmail.com>
2018-03-23 18:15:07 +0100

    PersistanceLayer
    Rykket fra main repository

---
+++ PersistanceLayer/DocumentDB/DBInterface/DAL/IRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+
+namespace DBInterface.DAL
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        /// <summary>
+        /// Returns an Enumerable of the items
+        /// </summary>
+        /// <param name="predicate">the specifications for the items</param>
+        /// <returns></returns>
+        Task<IEnumerable<T>> GetItemsAsync(Expression<Func<T, bool>> predicate);
+        /// <summary>
+        /// Creates a new item in the database
+        /// </summary>
+        /// <param name="item">The instance of the item</param>
+        /// <returns></returns>
+        Task<Document> CreateItemAsync(T item);
+        /// <summary>
+        /// Replace the item 
+        /// </summary>
+        /// <param name="id"> id of item</param>
+        /// <param name="item">replacing item</param>
+        /// <returns></returns>
+        Task<Document> UpdateItemAsync(string id, T item);
+        /// <summary>
+        /// Deleting an item from the database
+        /// </summary>
+        /// <param name="id">id of the item to delete</param>
+        /// <returns></returns>
+        Task DeleteItemAsync(string id);
+        /// <summary>
+        /// Returns a desired item from the database
+        /// </summary>
+        /// <param name="id">the id of of the item</param>
+        /// <returns></returns>
+        Task<T> GetItemAsync(string id);
+        /// <summary>
+        /// setup the db and documents
+        /// </summary>
+        void Initialize(); 
+        //IEnumerable<T> GetAll();
+        //IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+        //T Single(Expression<Func<T, bool>> predicate);
+        //T SingleOrDefault(Expression<Func<T, bool>> predicate);
+        //T First(Expression<Func<T, bool>> predicate);
+        //T GetById(int id);
+
+        //void Add(T entity);
+        //void Delete(T entity);
+        //void Attach(T entity);
+    }
+}
---
+++ PersistanceLayer/DocumentDB/Testing/Program.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+//hjælp fra nettet
+//https://docs.microsoft.com/en-us/azure/cosmos-db/sql-api-get-started
+
+//Account name: localhost:<port>
+//    Account key: C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==
+namespace Testing
+{
+    public class Program
+    {
+        #region Setups
+        private const string EndpointUrl = "https://localhost:8081";
+        private const string PrimaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+        private DocumentClient client;
+
+        #endregion
+
+        #region Main
+        private static void Main(string[] args)
+        {
+            try
+            {
+                Program p = new Program();
+                p.GetStartedDemo().Wait();
+            }
+            catch (DocumentClientException de)
+            {
+                Exception baseException = de.GetBaseException();
+                Console.WriteLine("{0} error occurred: {1}, Message: {2}", de.StatusCode, de.Message, baseException.Message);
+            }
+            catch (Exception e)
+            {
+                Exception baseException = e.GetBaseException();
+                Console.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of demo, press any key to exit.");
+                Console.ReadKey();
+            }
+
+        }
+        #endregion
+
+        #region Database operations
+        private async Task GetStartedDemo()
+        {
+            //point client to Database service
+            this.client = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
+            //Create Database FamilyDB
+            await this.client.CreateDatabaseIfNotExistsAsync(new Database { Id = "FamilyDB" });
+            //create collection FamilyCollection on FamilyDB
+            await this.client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri("FamilyDB"), new DocumentCollection { Id = "FamilyCollection" });
+
+            //Setup and implement documents
+            Family andersenFamily = new Family
+            {
+                Id = "Andersen.1",
+                LastName = "Andersen",
+                Parents = new Parent[]
+        {
+                new Parent { FirstName = "Thomas" },
+                new Parent { FirstName = "Mary Kay" }
+        },
+                Children = new Child[]
+        {
+                new Child
+                {
+                        FirstName = "Henriette Thaulow",
+                        Gender = "female",
+                        Grade = 5,
+                        Pets = new Pet[]
+                        {
+                                new Pet { GivenName = "Fluffy" }
+                        }
+                }
+        },
+                Address = new Address { State = "WA", County = "King", City = "Seattle" },
+                IsRegistered = true
+            };
+
+            await this.CreateFamilyDocumentIfNotExists("FamilyDB", "FamilyCollection", andersenFamily);
+
+            Family wakefieldFamily = new Family
+            {
+                Id = "Wakefield.7",
+                LastName = "Wakefield",
+                Parents = new Parent[]
+                    {
+                new Parent { FamilyName = "Wakefield", FirstName = "Robin" },
+                new Parent { FamilyName = "Miller", FirstName = "Ben" }
+                    },
+                Children = new Child[]
+                    {
+                new Child
+                {
+                        FamilyName = "Merriam",
+                        FirstName = "Jesse",
+                        Gender = "female",
+                        Grade = 8,
+                        Pets = new Pet[]
+                        {
+                                new Pet { GivenName = "Goofy" },
+                                new Pet { GivenName = "Shadow" }
+                        }
+                },
+                new Child
+                {
+                        FamilyName = "Miller",
+                        FirstName = "Lisa",
+                        Gender = "female",
+                        Grade = 1
+                }
+                    },
+                Address = new Address { State = "NY", County = "Manhattan", City = "NY" },
+                IsRegistered = false
+            };
+
+            await this.CreateFamilyDocumentIfNotExists("FamilyDB", "FamilyCollection", wakefieldFamily);
+
+            //query
+            this.ExecuteSimpleQuery("FamilyDB", "FamilyCollection");
+
+            //// Update the Grade of the Andersen Family child
+            //andersenFamily.Children[0].Grade = 6;
+            //await this.ReplaceFamilyDocument("FamilyDB", "FamilyCollection", "Andersen.1", andersenFamily);
+            ////query
+            //this.ExecuteSimpleQuery("FamilyDB", "FamilyCollection");
+
+            ////delete document
+            //await this.DeleteFamilyDocument("FamilyDB", "FamilyCollection", "Andersen.1");
+
+            //// Clean up/delete the database
+            //await this.client.DeleteDatabaseAsync(UriFactory.CreateDatabaseUri("FamilyDB"));
+        }
+        #endregion
+
+        #region WriteToConsole
+
+        private void WriteToConsoleAndPromptToContinue(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
+        }
+
+        #endregion
+
+        #region FamilyModel
+        public class Family
+        {
+            [JsonProperty(PropertyName = "id")]
+            public string Id { get; set; }
+            public string LastName { get; set; }
+            public Parent[] Parents { get; set; }
+            public Child[] Children { get; set; }
+            public Address Address { get; set; }
+            public bool IsRegistered { get; set; }
+            public override string ToString()
+            {
+                return JsonConvert.SerializeObject(this);
+            }
+        }
+        public class Parent
+        {
+            public string FamilyName { get; set; }
+            public string FirstName { get; set; }
+        }
+
+        public class Child
+        {
+            public string FamilyName { get; set; }
+            public string FirstName { get; set; }
+            public string Gender { get; set; }
+            public int Grade { get; set; }
+            public Pet[] Pets { get; set; }
+        }
+
+        public class Pet
+        {
+            public string GivenName { get; set; }
+        }
+
+        public class Address
+        {
+            public string State { get; set; }
+            public string County { get; set; }
+            public string City { get; set; }
+        }
+        #endregion
+
+        #region CreateDocument
+        private async Task CreateFamilyDocumentIfNotExists(string databaseName, string collectionName, Family family)
+        {
+            try
+            {
+                await this.client.ReadDocumentAsync(UriFactory.CreateDocumentUri(databaseName, collectionName, family.Id));
+                this.WriteToConsoleAndPromptToContinue("Found {0}", family.Id);
+            }
+            catch (DocumentClientException de)
+            {
+                if (de.StatusCode == HttpStatusCode.NotFound)
+                {
+                    await this.client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(databaseName, collectionName), family);
+                    this.WriteToConsoleAndPromptToContinue("Created Family {0}", family.Id);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        #endregion
+
+        #region SimpleQuery
+        private void ExecuteSimpleQuery(string databaseName, string collectionName)
+        {
+            // Set some common query options
+            FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1 };
+
+            // Here we find the Andersen family via its LastName
+            IQueryable<Family> familyQuery = this.client.CreateDocumentQuery<Family>(
+                    UriFactory.CreateDocumentCollectionUri(databaseName, collectionName), queryOptions)
+                .Where(f => f.LastName == "Andersen");
+
+            // The query is executed synchronously here, but can also be executed asynchronously via the IDocumentQuery<T> interface
+            Console.WriteLine("Running LINQ query...");
+            foreach (Family family in familyQuery)
+            {
+                Console.WriteLine("\tRead {0}", family);
+            }
+
+            // Now execute the same query via direct SQL
+            IQueryable<Family> familyQueryInSql = this.client.CreateDocumentQuery<Family>(
+                UriFactory.CreateDocumentCollectionUri(databaseName, collectionName),
+                "SELECT * FROM Family WHERE Family.LastName = 'Andersen'",
+                queryOptions);
+
+            Console.WriteLine("Running direct SQL query...");
+            foreach (Family family in familyQueryInSql)
+            {
+                Console.WriteLine("\tRead {0}", family);
+            }
+
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
+        }
+
+        #endregion
+
+        #region ReplaceDocuments
+        private async Task ReplaceFamilyDocument(string databaseName, string collectionName, string familyName, Family updatedFamily)
+        {
+            await this.client.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(databaseName, collectionName, familyName), updatedFamily);
+            this.WriteToConsoleAndPromptToContinue("Replaced Family {0}", familyName);
+        }
+
+        #endregion
+
+        #region Deletion
+        private async Task DeleteFamilyDocument(string databaseName, string collectionName, string documentName)
+        {
+            await this.client.DeleteDocumentAsync(UriFactory.CreateDocumentUri(databaseName, collectionName, documentName));
+            Console.WriteLine("Deleted Family {0}", documentName);
+        }
+
+        #endregion
+    }
+
+}
