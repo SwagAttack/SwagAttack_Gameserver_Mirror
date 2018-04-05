38475127d345769239341ab46b1d1638f69064d2
nicho1991 <ladefoged1991@gmail.com>
2018-04-05 16:07:56 +0200

    DomainLayer
    Usermodel to .net core

---
+++ DomainLayer/Old/Model.Unit.Test/User/ValidateGivenName.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Models.Interfaces;
+using NUnit.Framework;
+
+namespace Model.Unit.Test.User
+{
+    [TestFixture]
+    class ValidateGivenName
+    {
+        private IUser uut;
+
+        [SetUp]
+        public void setup()
+        {
+            uut = new Models.User.User();
+        }
+
+        [Test]
+        public void Email0Characters_ThrowsArgumentException()
+        {
+            Assert.That(() => uut.GivenName = "",
+                Throws.TypeOf<ArgumentException>().With.Message.EqualTo("value cannot be empty!"));
+            Assert.That(() => uut.GivenName == null);
+        }
+
+        [Test]
+        public void GivenName1Character_ThrowsArgumentException()
+        {
+            Assert.That(() => uut.GivenName = "A",
+                Throws.TypeOf<ArgumentException>().With.Message.EqualTo("Initials is not allowed"));
+            Assert.That(() => uut.GivenName == null);
+        }
+
+        [Test]
+        public void GivenNameInitial_ThrowsArgumentException()
+        {
+            Assert.That(() => uut.GivenName = "A.",
+                Throws.TypeOf<ArgumentException>().With.Message.EqualTo("Initials is not allowed"));
+            Assert.That(() => uut.GivenName == null);
+        }
+
+        [Test]
+        public void GivenName2Character_ThrowsNothing()
+        {
+            Assert.That(() => uut.GivenName = "Bo",
+                Throws.Nothing);
+            Assert.That(() => uut.GivenName == "Bo");
+        }
+
+        [Test]
+        public void GivenNameSmall_ThrowsNothing()
+        {
+            Assert.That(() => uut.GivenName = "bo",
+                Throws.Nothing);
+            Assert.That(() => uut.GivenName == "Bo");
+        }
+
+        [Test]
+        public void GivenNameWithNumbers_ThrowsArgumentException()
+        {
+            Assert.That(() => uut.GivenName = "1bo", Throws.TypeOf<ArgumentException>().With.Message.EqualTo("Name can only contain letters"));
+
+        }
+
+        [Test]
+        public void GivenNameWithSpecial_ThrowsArgumentException()
+        {
+            Assert.That(() => uut.GivenName = "%bo", Throws.TypeOf<ArgumentException>().With.Message.EqualTo("Name can only contain letters"));
+
+        }
+
+    }
+}
