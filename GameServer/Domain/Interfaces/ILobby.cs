7c1536ace077e93631c19c8195cf53ff7d178bb1
Malthe <malthedj@hotmail.com>
2018-04-11 13:16:57 +0200

    added first draft to Ilobby and Lobby

---
+++ GameServer/Domain/Interfaces/ILobby.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Domain.Interfaces
+{
+    public interface ILobby
+    {
+        IReadOnlyCollection<string> Usernames { get; }
+        string AdminUserName { get; set; }
+        
+        void AddUser(IUser user);
+        void RemoveUser(IUser user);
+
+    }
+}
---
+++ GameServer/Domain/Models/Lobby.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Domain.Interfaces;
+
+namespace Domain.Models
+{
+    public class Lobby : ILobby
+    {
+        private readonly Collection<string> _usernames;
+
+        public IReadOnlyCollection<string> Usernames => _usernames;
+
+        public string AdminUserName { get; set; }
+
+        public Lobby(IUser user)
+        {
+            AdminUserName = user.Username;
+            _usernames = new Collection<string>();
+            AddUser(user);
+        }
+
+        public void AddUser(IUser user)
+        {
+            _usernames.Add(user.Username);
+        }
+
+        public void RemoveUser(IUser user)
+        {
+            _usernames.Remove(user.Username);
+        }
+    }
+}
