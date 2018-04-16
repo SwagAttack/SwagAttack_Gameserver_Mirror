10dd88a3921d229f7e1857026a755a4a0dca38a1
Gobbenobber <gobbenobber@gmail.com>
2018-04-16 14:46:47 +0200

    Added Async methods and wrote unit tests for these.

--- GameServer/Persistance.Test.Unittests/PersistanceLayerUnitTests.cs
+++ GameServer/Persistance.Test.Unittests/PersistanceLayerUnitTests.cs
@@ -35,12 +35,41 @@ namespace Persistance.Test.Unittests
         }
 
         [Test]
+        public void AddUserToDbAsync_UserIsAddedToDb()
+        {
+            var user = _uut.UserRepository.AddUserAsync(_testUser);
+            user.Wait();
+            Assert.That(user.Result.Username,Is.EqualTo(_testUser.Username));
+            _addedUser = true;
+        }
+
+        [Test]
         public void ReplaceUserInDb_UserIsReplacedInDb()
         {
             _uut.UserRepository.AddUser(_testUser);
             _testUser.GivenName = "Replaced";
             _uut.UserRepository.ReplaceUser(_testUser);
             Assert.That(_testUser.GivenName == _uut.UserRepository.GetUserByUsername(_testUser.Username).GivenName);
+            _addedUser = true;
+        }
+
+        [Test]
+        public void ReplaceUserInDbAsync_UserIsReplacedInDb()
+        {
+            _uut.UserRepository.AddUser(_testUser);
+            _testUser.GivenName = "Replaced";
+            var replacedUser = _uut.UserRepository.ReplaceUserAsync(_testUser.Username, _testUser);
+            replacedUser.Wait();
+            Assert.That(replacedUser.Result.GivenName,Is.EqualTo(_testUser.GivenName));
+            _addedUser = true;
+        }
+
+        [Test]
+        public void FindUserInDb_UserExists()
+        {
+            _uut.UserRepository.AddUser(_testUser);
+            var user = _uut.UserRepository.GetUserByUsername(_testUser.Username);
+            Assert.That(user.Username == _testUser.Username);
             _addedUser = true;
         }
 
@@ -55,21 +84,20 @@ namespace Persistance.Test.Unittests
         }
 
         [Test]
-        public void FindUserInDb_UserExists()
-        {
-            _uut.UserRepository.AddUser(_testUser);
-            var user = _uut.UserRepository.GetUserByUsername(_testUser.Username);
-            Assert.That(user.Username == _testUser.Username);
-            _addedUser = true;
-        }
-
-        [Test]
         public void DeleteUserInDb_UserDoesNotExistPostExecution()
         {
             _uut.UserRepository.AddUser(_testUser); // put it up again
             _uut.UserRepository.DeleteUserByUsername(_testUser.Username);
             Assert.That(_uut.UserRepository.GetUserByUsername(_testUser.Username), Is.Null);
 
+        }
+
+        [Test]
+        public void DeleteUserInDbAsync_UserDoesNotExistPostExecution()
+        {
+            _uut.UserRepository.AddUser(_testUser); // put it up again
+            _uut.UserRepository.DeleteUserByUsernameAsync(_testUser.Username).Wait();
+            Assert.That(_uut.UserRepository.GetUserByUsername(_testUser.Username), Is.Null);
         }
 
 
--- GameServer/Persistance/Repositories/IUserRepository.cs
+++ GameServer/Persistance/Repositories/IUserRepository.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 using Domain.Interfaces;
 
 namespace Persistance.Repositories
@@ -14,21 +17,49 @@ namespace Persistance.Repositories
         /// <summary>
         /// Given the username, a user can be found
         /// </summary>
-        /// <param name="id">The string that is the username</param>
+        /// <param name="username">The string that is the username</param>
         /// <returns>The user that was found</returns>
-        IUser GetUserByUsername(string id);
+        IUser GetUserByUsername(string username);
         /// <summary>
         /// Deletion in the database
         /// </summary>
         /// <param name="username">The name of the user to be deleted</param>
         void DeleteUserByUsername(string username);
         /// <summary>
-        /// for updates to a user.
+        /// For updates to a user.
         /// </summary>
         /// <param name="thisUser">the updated user</param>
         void ReplaceUser(IUser thisUser);
-
-        Task<IUser> GetUserByUsernameAsync(string id);
+        /// <summary>
+        /// Given the username, a user can be found.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns>The user that was found</returns>
+        Task<IUser> GetUserByUsernameAsync(string username);
+        /// <summary>
+        /// Might not be needed
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns>Users that were found to match the predicate expression</returns>
+        Task<IEnumerable<IUser>> GetUsersByUsernameAsync(Expression<Func<IUser, bool>> predicate);
+        /// <summary>
+        /// Updates a user with a given username with the new user information.
+        /// </summary>
+        /// <param name="username">Username of the user to update/replace</param>
+        /// <param name="user">The replacement user.</param>
+        /// <returns>The updated user.</returns>
+        Task<IUser> ReplaceUserAsync(string username, IUser user);
+        /// <summary>
+        /// Adds a user to the database.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>The user that was added.</returns>
+        Task<IUser> AddUserAsync(IUser user);
+        /// <summary>
+        /// Given a username, delete user in DB with mathing username.
+        /// </summary>
+        /// <param name="username">The username of the user to delete.</param>
+        Task DeleteUserByUsernameAsync(string username);
 
     }
 }
