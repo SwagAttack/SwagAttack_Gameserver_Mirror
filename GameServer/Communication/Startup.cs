e71984f023fcfca7833fbc656f60b062fb214408
jonasna <jonasna93@gmail.com>
2018-04-19 12:37:22 +0200

    Modified master to be https

--- GameServer/Communication/Startup.cs
+++ GameServer/Communication/Startup.cs
@@ -8,7 +8,8 @@ using Application.Managers;
 using Application.Misc;
 using Persistance.UnitOfWork;
 using Persistance;
-using Communication.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 
 namespace Communication
 {
@@ -26,8 +27,7 @@ namespace Communication
         {
             services.AddMvc(options =>
             {
-                //options.Filters.Add(typeof(ValidateModelStateAttribute));
-                //options.Filters.Add(new RequireHttpsAttribute());
+                options.Filters.Add(new RequireHttpsAttribute());
             });
            
             services.AddTransient<IUnitOfWork>(u => new UnitOfWork(new DbContext()));
@@ -43,8 +43,8 @@ namespace Communication
                 app.UseDeveloperExceptionPage();
             }
 
-            //var options = new RewriteOptions().AddRedirectToHttps();
-            //app.UseRewriter(options);
+            var options = new RewriteOptions().AddRedirectToHttps();
+            app.UseRewriter(options);
 
             app.UseMvc();
         }
