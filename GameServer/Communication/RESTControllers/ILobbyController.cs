7ede0bdc2e24575aec81c36c4b3d6f73c973a8ce
Katborg <s.katborg@gmail.com>
2018-04-25 20:16:43 +0200

    rest api Lobby controller

--- GameServer/Communication/RESTControllers/ILobbyController.cs
+++ GameServer/Communication/RESTControllers/ILobbyController.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Web.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Communication.RESTControllers
 {
 	public interface ILobbyController
 	{
-		IActionResult GetAllLobby([FromHeader] string username, [FromHeader] string password);
-		IActionResult PutRemoveUser([FromHeader] string username, [FromHeader] string password, string LobbyId);
-		IActionResult PutUserIn([FromHeader] string username, [FromHeader] string password, string LobbyId);
-		IActionResult PostCreateLobby([FromHeader] string username, [FromHeader] string password, string LobbyId);
+		IActionResult GetAllLobby([FromHeader] string username);
+		IActionResult GetLobbyById([FromHeader] string username, [FromHeader] string LobbyId);
+		IActionResult PostCreateLobby([FromHeader] string username, [FromUri] string LobbyId);
+		IActionResult PutRemoveUser([FromHeader] string username, [FromUri] string LobbyId);
+		IActionResult PutUserIn([FromHeader] string username, [FromUri] string LobbyId);
 	}
 }
