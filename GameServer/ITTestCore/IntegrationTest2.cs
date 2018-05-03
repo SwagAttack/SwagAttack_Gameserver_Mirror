2b0d81c751f06d57de0d661f8d2a95cd8d2d3134
Malthe <malthedj@hotmail.com>
2018-05-03 19:16:29 +0200

    updated integrationTest1 and refactored.

--- GameServer/ITTestCore/IntegrationTest2.cs
+++ GameServer/ITTestCore/IntegrationTest2.cs
@@ -5,20 +5,68 @@ using Application.Controllers;
 using Application.Interfaces;
 using Application.Managers;
 using Application.Misc;
+using Communication.Formatters;
 using Communication.RESTControllers;
+using Domain.DbAccess;
 using Domain.Interfaces;
 using Domain.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Azure.Documents.SystemFunctions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using NSubstitute;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using Persistance;
+using Persistance.Interfaces;
+using Persistance.Setup;
+using LobbyController = Application.Controllers.LobbyController;
 using UserController = Application.Controllers.UserController;
 
 namespace IT_Core
 {
+    public class IntegrationTest2Startup
+    {
+        public IDbContext FakeDbContext = Substitute.For<IDbContext>();
+        public IUserRepository FakeUserRepository = Substitute.For<IUserRepository>();
 
+        public IntegrationTest2Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options =>
+            {
+                options.InputFormatters.Insert(0, new JsonInputFormatter());
+            });
+
+            // Database fake
+            services.AddSingleton<IDbContext>(provider => FakeDbContext);
+
+
+            // Usercollection fake
+            services.AddTransient<IUserRepository>(provider => FakeUserRepository);
+
+            services.AddSingleton(l => LoginManager.GetInstance());
+            services.AddSingleton<ILobbyController, LobbyController>();
+            services.AddTransient<IUserController, UserController>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseMvc();
+        }
+    }
     //[TestFixture] <--- Er blevet udkommenteret, da en textFixture uden tests får Travis til at fejle
     public class IntegrationTest2
     {
@@ -34,10 +82,6 @@ namespace IT_Core
         private Mock<UnitOfWork> OUWM = new Mock<UnitOfWork>();
         private Application.Controllers.UserController _uut;
 */
-
-
-
-
 
         [SetUp]
         public void Setup()
@@ -62,19 +106,10 @@ namespace IT_Core
         //*************************Test App Layer***************************
 
         //Test if we send usercontroler in applicationLayer gets received in persistance Layer
-        [Test]
-        public void IT_2_GS_LoginUser()
-        {
-          
-        }
+
 
         //Test if Createuser with pers as userobj, gets received in persistance Layer
-        [Test]
-        public void IT_2_GS_CreateUser()
-        {
-            
 
-        }
 
 
 
