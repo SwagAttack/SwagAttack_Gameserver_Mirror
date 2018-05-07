f7665ff8fbe04b6fd0f6d2b9e563495f4daf8124
jonasna <jonasna93@gmail.com>
2018-05-07 06:25:40 +0200

    More time

---
+++ GameServer/Application.Test.Unittests/UserCacheTestSetup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Application.Test.Unittests
+{
+    internal static class FakeUserGenerator
+    {
+        private static string GenerateRandomString()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            return new string(stringChars);
+        }
+
+        private static string Pass = "ADSAD";
+
+        public static Dictionary<string, string> GenerateFakeUsers(int count)
+        {
+            var users = new Dictionary<string, string>();
+
+            for (var i = 0; i < count; i++)
+            {
+                users[GenerateRandomString()] = Pass;
+            }
+
+            return users;
+        }
+    }
+    [SetUpFixture]
+    public class UserCacheTestSetup
+    {
+        public static Dictionary<string, string> FakeUsers { get; set; }
+        [OneTimeSetUp]
+        public void BeforeAnyTest()
+        {
+            FakeUsers = FakeUserGenerator.GenerateFakeUsers(50000);
+        }
+    }
+}
--- GameServer/Application.Test.Unittests/UserCacheUnitTests.cs
+++ GameServer/Application.Test.Unittests/UserCacheUnitTests.cs
@@ -13,37 +13,6 @@ using NUnit.Framework.Internal.Execution;
 
 namespace Application.Test.Unittests
 {
-    internal static class FakeUserGenerator
-    {
-        private static string GenerateRandomString()
-        {
-            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            var stringChars = new char[8];
-            var random = new Random();
-
-            for (int i = 0; i < stringChars.Length; i++)
-            {
-                stringChars[i] = chars[random.Next(chars.Length)];
-            }
-
-            return new string(stringChars);
-        }
-
-        private static string Pass = "ADSAD";
-
-        public static Dictionary<string, string> GenerateFakeUsers(int count)
-        {
-            var users = new Dictionary<string, string>();
-            
-            for (var i = 0; i < count; i++)
-            {
-                users[GenerateRandomString()] = Pass;
-            }
-
-            return users;
-        }
-    }
-
     [TestFixture]
     public class UserCacheUnitTests
     {
@@ -66,7 +35,7 @@ namespace Application.Test.Unittests
         private DateTime _twentyMinutesFromNowLittleMore;
         private DateTime _twentyMinutesFromNowLittleLess;
 
-        private Dictionary<string, string> FakeUsers { get; set; }
+        private Dictionary<string, string> FakeUsers;
 
         private bool TimeStampIsUpdatedAccordingToNow(DateTime dateTime)
         {
@@ -75,17 +44,12 @@ namespace Application.Test.Unittests
             return (dateTime.CompareTo(_twentyMinutesFromNowLittleMore) < 0) && (dateTime.CompareTo(_twentyMinutesFromNowLittleLess) > 0);
         }
 
-        [OneTimeSetUp]
-        public void BeforeAnyTest()
-        {
-            FakeUsers = FakeUserGenerator.GenerateFakeUsers(50000);
-        }
-
         [SetUp]
         public void SetUp()
         {
             _fakeTimer = Substitute.For<ITimer>();
             _uut = new UserCache(_fakeTimer);
+            FakeUsers = UserCacheTestSetup.FakeUsers;
         }
 
         [Test]
